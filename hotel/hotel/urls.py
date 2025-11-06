ef5b378aa481c433e7342633faf937971cf4ea80
Nadir <nadiradad@gmail.com>
2025-11-06 19:59:31 -0300

    haciendo andar las imagenes

--- hotel/hotel/urls.py
+++ hotel/hotel/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -25,3 +27,6 @@ urlpatterns = [
     path('usuarios/', include('apps.usuarios.urls')),
     path('reservas/', include('apps.reservas.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
